edcba88a9faedd2c541da6c9d56efdccba9c7b89
mrheg <mrheggie.1@icloud.com>
2024-11-20 17:12:50 +1000

    got rid of headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack
+  screenOptions={{
+    // Hide the header for all other routes.
+    headerShown: false,
+  }}
+>
+  <Stack.Screen
+    name="index"
+    options={{
+      // Hide the header for this route
+      headerShown: false,
+    }}
+  />
+</Stack>;
 }
