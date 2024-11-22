0f67f2a758c4dc4dc70ff1f98876166682084fa3
aztec-dev <aztech.dev1@gmail.com>
2024-11-22 17:30:32 +1000

    wip: added app bar nav

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,27 @@
 import { Stack } from "expo-router";
+import Game from "./game/game-board";
 
 export default function RootLayout() {
   return <Stack
   screenOptions={{
     // Hide the header for all other routes.
-    headerShown: false,
+    headerShown: true,
   }}
 >
   <Stack.Screen
     name="index"
     options={{
       // Hide the header for this route
-      headerShown: false,
+      headerShown: true,
+      title: 'Menu',
+      headerTitleAlign: 'center',
+    }}
+  />
+  <Stack.Screen
+    name="Game"
+    // component={Game}
+    options={{
+      title: "Game"
     }}
   />
 </Stack>;
