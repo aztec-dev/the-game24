5b955dceedc7058ece1b1ea84319920ef8f64364
aztec-dev <aztech.dev1@gmail.com>
2024-11-22 16:18:53 +1000

    feat: add play button component

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,27 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import {Link} from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Button from "./menu/Button";
+
+// Main starting screen that displays buttons mapped to 
+// files that displays different screens.
 
 export default function Index() {
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Link href="/game-board" style={{color: 'green'}}> Game</Link>
-    </View>
+      <Link href="/game/game-board" style={{color: 'green'}}> Game</Link>
+      <Button
+      onPress={() => console.log('Button pressed')}
+      title="Play"
+      href="/game/game-board"
+      ></Button>
+    </SafeAreaView>
   );
+
 }
