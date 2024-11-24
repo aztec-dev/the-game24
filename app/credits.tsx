2bec97c4187f849d8bbacd8649936b1705d18ec0
mrheg <mrheggie.1@icloud.com>
2024-11-25 07:42:31 +1000

    credits page added

---
+++ app/credits.tsx
@@ -0,0 +1,17 @@
+import { Component, useState } from "react";
+import { Text, View, StyleSheet, Linking, ScrollView, TouchableOpacity, Dimensions } from "react-native";
+import {Link} from 'expo-router';
+
+const guide = () => {
+    return (
+        <View style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 40
+            }}>
+            <Text> Placeholder </Text>
+        </View>
+    )
+}
+
+export default guide;
